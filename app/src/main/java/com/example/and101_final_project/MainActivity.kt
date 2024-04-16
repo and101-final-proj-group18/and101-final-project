c334503e0460d94f5b1783a8fcc5f0d95387a0a9
PapiChuloXX <afrokid010404@gmail.com>
2024-04-16 00:28:48 -0500

    Added logic for splash screen and change of state in search bar

--- app/src/main/java/com/example/and101_final_project/MainActivity.kt
+++ app/src/main/java/com/example/and101_final_project/MainActivity.kt
@@ -4,11 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
