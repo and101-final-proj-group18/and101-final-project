95b36816b40ea65858520114af60411b20a874e4
PapiChuloXX <afrokid010404@gmail.com>
2024-04-17 12:03:43 -0500

    added price to the Restaurant data class

--- app/src/main/java/com/example/nearbyeats/Restaurant.kt
+++ app/src/main/java/com/example/nearbyeats/Restaurant.kt
@@ -2,10 +2,11 @@ package com.example.nearbyeats
 
 data class Restaurant (
     val name: String,
+    val image_url: String,
+    val address: String,
     val latitude: Double,
     val longitude: Double,
     val ratings: Double,
-    val reviews: Int,
-    val image_url: String,
-    val address: String
+    val price: Double,
+    val reviews: Int
 )
